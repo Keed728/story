71e4e9a48b9620303d3e30ff90fd5874f67815c0
Keed728 <keerthana728driya@gmail.com>
2024-11-10 11:38:57 +0530

    build 2

---
+++ src/main/java/facebook/App.java
@@ -0,0 +1,15 @@
+package facebook;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int a=3;
+        int b=77;
+        int sub=a+b;
+         System.out.println(sub);
+
+    }
+}
